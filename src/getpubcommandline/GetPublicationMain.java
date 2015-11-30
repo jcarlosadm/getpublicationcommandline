df538af84243ebe8f461b1515942d226fcebd7b4
jcarlos <carlosmaster10@gmail.com>
2015-11-30 20:49:18 -0300

    GetPublicationMain updated

--- src/getpubcommandline/GetPublicationMain.java
+++ src/getpubcommandline/GetPublicationMain.java
@@ -7,19 +7,26 @@ import getpubcommandline.plugin.CommandLineSitePlugin;
 import getpubcommandline.ui.commands.ContextCommand;
 import getpubcommandline.ui.commands.implementations.SiteChooserCommand;
 import getpublication.folders.UserFolder;
+import getpublication.util.folder.CreateFolder;
 import net.xeoh.plugins.base.PluginManager;
 import net.xeoh.plugins.base.impl.PluginManagerFactory;
 import net.xeoh.plugins.base.util.PluginManagerUtil;
 
 public class GetPublicationMain {
+
+    private static final File PLUGIN_FOLDER = new File(
+            UserFolder.getPathToPluginFolder() + File.separator
+                    + "commandLine");
+
     public static void main(String[] args) {
+        CreateFolder.create(PLUGIN_FOLDER);
+
         PluginManager pluginManager = PluginManagerFactory
                 .createPluginManager();
-        pluginManager.addPluginsFrom(
-                new File(UserFolder.getPathToPluginFolder()).toURI());
+        pluginManager.addPluginsFrom(PLUGIN_FOLDER.toURI());
 
-        Collection<CommandLineSitePlugin> plugins = new PluginManagerUtil(pluginManager)
-                .getPlugins(CommandLineSitePlugin.class);
+        Collection<CommandLineSitePlugin> plugins = new PluginManagerUtil(
+                pluginManager).getPlugins(CommandLineSitePlugin.class);
 
         ContextCommand contextCommand = new ContextCommand();
         SiteChooserCommand command = new SiteChooserCommand();
